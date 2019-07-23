5314259c539a7c29ad634e442e326b707abcfec8
Luke Williams <luke@turbox.uk>
2019-07-23 19:25:07 +0100

    aex dt

--- clone.sh
+++ clone.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 # write commands if you want to clone some repos
+
+mkdir .repo/local_manifests
+cd .repo/local_manifests
+wget https://gitlab.com/ExpressLuke/local_manifests/raw/aex/roomservice.xml
+cd ../..
