8152f1f7356e52821c6ee2b1621291bc78f19354
Luke Williams <expressluke@turbox.uk>
2019-07-24 04:12:15 +0100

    crdroid pie for surnz

--- config.sh
+++ config.sh
@@ -9,10 +9,10 @@ KBUILD_BUILD_HOST="ExpressDrone"
 export oem=motorola
 export device=surnia
 
-ROM="PixelExperience"
-manifest_url="https://github.com/PixelExperience/manifest"
-export rom_vendor_name="aosp" # This represent the nams used by different rom vendors, Ex - aosp_harpia-userdebug, aosp is vendor name.
-branch="pie"
+ROM="CrDroid"
+manifest_url="https://github.com/crdroidandroid/android"
+export rom_vendor_name="lineage" # This represent the nams used by different rom vendors, Ex - aosp_harpia-userdebug, aosp is vendor name.
+branch="9.0"
 
 release_repo="ExpressLuke/github-releases"
 
