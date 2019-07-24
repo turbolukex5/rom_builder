898ba9c615322021ca300e2976492edaaf1848e0
Luke Williams <luke@turbox.uk>
2019-07-24 01:23:22 +0100

    Old trees
    welp

--- clone.sh
+++ clone.sh
@@ -2,10 +2,10 @@
 
 # write commands if you want to clone some repos
 
-git clone https://github.com/aosp4surnia/device_motorola_surnia-1 -b pie device/motorola/surnia
-git clone https://gitlab.com/surnia-development/surnia-pie/device_motorola_msm8916-common -b lineage-16.0-chil device/motorola/msm8916-common
-git clone https://github.com/MinimalAndroidVendor/proprietary_vendor_motorola_msm8916 -b lineage-16.0-chil vendor/motorola
+git clone https://github.com/aosp4surnia/device_motorola_surnia -b pe device/motorola/surnia
+git clone https://github.com/aosp4surnia/android_device_motorola_msm8916-common -b lineage-16.0 device/motorola/msm8916-common
+git clone https://github.com/aosp4surnia/proprietary_vendor_motorola -b lineage-16.0 vendor/motorola
 git clone https://github.com/LineageOS/android_packages_resources_devicesettings -b lineage-15.1 packages/resoures/devicesettings
 git clone https://github.com/LineageOS/android_external_sony_boringssl-compat -b lineage-16.0 external/sony/boringssl-compat
 git clone https://github.com/LineageOS/android_system_qcom -b lineage-16.0 system/qcom
-git clone https://github.com/oreo-surnia/android_kernel_motorola_msm8916 -b lineage-16.0-squid kernel/motorola/msm8916
+git clone https://github.com/aosp4surnia/android_kernel_motorola_msm8916 -b pie-kornelio kernel/motorola/msm8916
