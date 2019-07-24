e418ee3202b59dee710872265757763a8fcc8390
Luke Williams <luke@turbox.uk>
2019-07-24 01:53:12 +0100

    wrong tree

--- clone.sh
+++ clone.sh
@@ -2,7 +2,7 @@
 
 # write commands if you want to clone some repos
 
-git clone https://github.com/aosp4surnia/device_motorola_surnia -b pe device/motorola/surnia
+git clone https://github.com/aosp4surnia/android_device_motorola_surnia -b pe device/motorola/surnia
 git clone https://github.com/aosp4surnia/android_device_motorola_msm8916-common -b lineage-16.0 device/motorola/msm8916-common
 git clone https://github.com/aosp4surnia/proprietary_vendor_motorola -b lineage-16.0 vendor/motorola
 git clone https://github.com/LineageOS/android_packages_resources_devicesettings -b lineage-15.1 packages/resoures/devicesettings
